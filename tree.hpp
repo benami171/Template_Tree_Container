30070d96bc37d28ac1063a66be0e589f8794fd7b
Gal Ben Ami <benami171@gmail.com>
2024-06-19 17:20:15 +0300

    init.

---
+++ tree.hpp
@@ -0,0 +1,7 @@
+// will be a generic class of a tree. 
+// it will have a constructor that gets int k = number of children each node can have at most. 
+// if k isnt stated when the constructor was called , set k = 2 by default.
+// the tree will contain nodes that contains templates of type T.
+// we will want to implement a container that represent the tree thats mentioned above.
+
+// when creating a tree, we will need to
